949800c595f5c23204c40eb7ae0e5ea4c00b1a85
sngtmth <sngtmth@gmail.com>
2021-09-24 00:36:27 +0530

    added features

--- src/JavaPrgm.java
+++ src/JavaPrgm.java
@@ -2,6 +2,8 @@
 public class JavaPrgm {
 	public static void main(String args[]) {
 		System.out.println("welcome");
+		System.out.println("HelloJava");
+		System.out.println("Welcome to java");
 	}
 
 }
